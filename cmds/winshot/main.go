52e420c6a8db3499f5d6e0842583be4d18b30c57
Anil Kumar <anil@browserstack.com>
2015-03-31 20:49:29 +0530

    fixing image

--- cmds/winshot/main.go
+++ cmds/winshot/main.go
@@ -5,12 +5,22 @@ import "fmt"
 import "image"
 import "image/jpeg"
 import "os"
+import "image/color"
+
+type myimg struct {
+	image.RGBA
+}
+
+func (m *myimg) At(x, y int) color.Color {
+	r, g, b, a := m.RGBA.At(x, m.Rect.Max.Y-y-1).RGBA()
+	return color.RGBA{uint8(b), uint8(g), uint8(r), uint8(a)}
+}
 
 func main() {
 	data, w, h, s := winshot.Raw()
 	fmt.Println(len(data), w, h, s)
 	fmt.Println("yo")
-	m := &image.RGBA{data, s, image.Rect(0, 0, w, h)}
+	m := &myimg{image.RGBA{data, s, image.Rect(0, 0, w, h)}}
     f, err := os.Create("some.jpg")
     if err != nil {
     	panic(err)

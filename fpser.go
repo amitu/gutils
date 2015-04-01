57b1f09192584eaca1fb9d56b08b2db7f1a06fae
Amit Upadhyay <amitu@browserstack.com>
2015-04-01 12:51:36 +0530

    minor

--- fpser.go
+++ fpser.go
@@ -1,9 +1,6 @@
 package gutils
 
-import (
-	"fmt"
-	"time"
-)
+import "time"
 
 // FPSer is used to keep things ticking at predefined FPS.
 type FPSer struct {
@@ -15,8 +12,6 @@ type FPSer struct {
 // Tick sleeps if required to keep the FPS.
 func (f *FPSer) Tick() {
 	delta := time.Since(f.last)
-	// 1m1.221363664s 200ms -1m1.021363664s 1e+09 5 ft
-	fmt.Println(delta, f.target, f.target-delta, float32(time.Second), f.fps, "ft")
 	if delta < f.target {
 		time.Sleep(f.target - delta)
 	}
@@ -26,6 +21,5 @@ func (f *FPSer) Tick() {
 // NewFPSer can be used to construct FPSser
 func NewFPSer(fps float32) *FPSer {
 	target := time.Duration(float32(time.Second) / fps)
-	fmt.Println(target)
-	return &FPSer{fps, time.Now().Add(-time.Minute), target}
+	return &FPSer{fps, time.Now().Add(-target), target}
 }

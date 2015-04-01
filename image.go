3dd2a2be7d201ae4d2408f67f25a8b38b8ffb878
Amit Upadhyay <amitu@browserstack.com>
2015-04-01 13:11:20 +0530

    image helpers

---
+++ image.go
@@ -0,0 +1,42 @@
+package gutils
+
+import (
+	"image"
+	"image/color"
+)
+
+// MacARGB is used to represent the image obtained from OS X screenbuffer.
+type MacARGB struct {
+	image.RGBA
+}
+
+// At can be used to find the color at given pixel. It has been modified to
+// accomodate the difference between image.RGBA way of storing pixel data and
+// the OS X way of storing pixel data
+func (m *MacARGB) At(x, y int) color.Color {
+	r, g, b, a := m.RGBA.At(x, y).RGBA()
+	return color.RGBA{uint8(b), uint8(g), uint8(r), uint8(a)}
+}
+
+// NewMacARGB returns a MacARGB object with given pixel data
+func NewMacARGB(r image.Rectangle, stride int, pix []byte) *MacARGB {
+	return &MacARGB{RGBA: image.RGBA{Pix: pix, Stride: stride, Rect: r}}
+}
+
+// WinBGRA represents the image obtained from windows framebuffer
+type WinBGRA struct {
+	image.RGBA
+}
+
+// At can be used to find the color at given pixel. It has been modified to
+// accomodate the difference between image.RGBA way of storing pixel data and
+// the Windows way of storing pixel data
+func (m *WinBGRA) At(x, y int) color.Color {
+	r, g, b, a := m.RGBA.At(x, m.Rect.Max.Y-y-1).RGBA()
+	return color.RGBA{uint8(b), uint8(g), uint8(r), uint8(a)}
+}
+
+// NewWinBGRA returns a MacARGB object with given pixel data
+func NewWinBGRA(r image.Rectangle, stride int, pix []byte) *MacARGB {
+	return &MacARGB{RGBA: image.RGBA{Pix: pix, Stride: stride, Rect: r}}
+}
